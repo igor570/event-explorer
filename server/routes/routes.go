23b3eb4c0bc664d6762b4ae1748a67e0b7b4c3d9
Igor Milosavljevic <91330526+igor570@users.noreply.github.com>
2024-07-11 11:04:08 +0100

    added USERS table

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -3,9 +3,15 @@ package routes
 import "github.com/gin-gonic/gin"
 
 func RegisterRoutes(app *gin.Engine) {
+
+	//Events
 	app.GET("/events", getEvents)
 	app.GET("/events/:id", getEventById)
 	app.POST("/events", createEvent)
 	app.PUT("/events/:id", updateEvent)
 	app.DELETE("/events/:id", deleteEvent)
+
+	//Authentication
+	app.POST("/signup")
+
 }

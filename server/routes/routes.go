049fb98dd010a8be5a6110381d0bd18283930de7
Igor Milosavljevic <91330526+igor570@users.noreply.github.com>
2024-07-20 13:25:37 +0100

    verified JWT Token
    - verified the JWT Token with a verify function
    - marked createEvent to only pass with verified token

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -7,6 +7,7 @@ func RegisterRoutes(app *gin.Engine) {
 	//Events
 	app.GET("/events", getEvents)
 	app.GET("/events/:id", getEventById)
+	//Protected Events Routes
 	app.POST("/events", createEvent)
 	app.PUT("/events/:id", updateEvent)
 	app.DELETE("/events/:id", deleteEvent)
--- server/utils/jwt.go
+++ server/utils/jwt.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -16,4 +17,41 @@ func CreateToken(email string, userId int64) (string, error) {
 	}) //new token with data
 
 	return token.SignedString([]byte(secretKey))
+}
+
+func VerifyToken(token string) error {
+	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok { //checking type signing method of token
+			return nil, errors.New("unexpected signing method")
+		}
+		return []byte(secretKey), nil
+	})
+
+	if err != nil {
+		return errors.New("could not parse the token")
+	}
+
+	tokenIsValid := parsedToken.Valid
+
+	if !tokenIsValid {
+		return errors.New("invalid token")
+	}
+
+	/*
+		//optional valdiation of properties in the token: email and user id
+
+		claims, ok := parsedToken.Claims.(jwt.MapClaims) //checking the type of parsedToken
+
+		if !ok {
+			return errors.New("Invalid token claims")
+		}
+
+		//accessing the data in a map
+		email := claims["email"].(string)
+		userId := claims["userId"].(int64)
+
+	*/
+
+	return nil
+
 }

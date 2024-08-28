794e6c93164341a16e34ba660f1040057695723b
Igor Milosavljevic <91330526+igor570@users.noreply.github.com>
2024-08-28 17:08:09 +0100

    Removed Client

--- client/src/lib/schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const UserFormSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(8),
-});
-
-export type UserFormValues = z.infer<typeof UserFormSchema>;
--- client/src/screens/SignUp.tsx
+++
@@ -1,9 +0,0 @@
-import { UserForm } from "./components/UserForm.tsx";
-
-export const SignUp = () => {
-  return (
-    <main className="flex flex-col items-center justify-center h-screen bg-zinc-950">
-      <UserForm />
-    </main>
-  );
-};
